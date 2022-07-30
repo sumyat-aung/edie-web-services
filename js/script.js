701c0e4774a20d52e291d7f616501aa02743c96f
sumyataung2004 <sumyataung1584@gmail.com>
2022-07-30 23:47:59 +0630

    5th commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-// scrolling to top btn -
-
-const scroll = document.getElementById("scroll");
-
-window.addEventListener("scroll", function () {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scroll.style.display = "block";
-  } else {
-    scroll.style.display = "none";
-  }
-});
-
-scroll.addEventListener("click", function () {
-  window.scrollTo(0, 0);
-});
-
 // responsive btn
 
 const resBtn = document.getElementById("res-btn");
